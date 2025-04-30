f4afbe64b9493fb42c3f4b04987f8904a42e30d2
NadiaEnoaRizoAvalos <Nadia.RizoAvalos@alu.frt.utn.edu.ar>
2025-04-30 18:59:05 -0300

    refactorice el codigo agregue herencia

---
+++ Domain/CajaDeAhorro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain
+{
+   public class CajaDeAhorro: CuentaBancaria
+    {
+       public decimal TasaDeInteres {  get; init; }
+       public CajaDeAhorro (int numero, decimal saldo) : base (numero, saldo) { }
+
+    }
+}
---
+++ Domain/CuentaCorriente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain
+{
+
+
+    public class CuentaCorriente : CuentaBancaria
+    {
+        public decimal LimiteDeDescubierto {  get; init; }
+        public decimal Comision {  get; set; }
+        public CuentaCorriente(int numero, decimal saldo ) : base(numero,saldo) { }
+    }
+
+
+}
