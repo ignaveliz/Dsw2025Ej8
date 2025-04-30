1b196e299a1e8839304b2170b09ad0929ffa74d5
Ignacio Veliz <ignaciomartin.veliz@alu.frt.utn.edu.ar>
2025-04-30 19:22:41 -0300

    Agregué excepciones propias para las reglas

---
+++ Exceptions/CuentaNoActiva.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain
+{
+    public class CuentaNoActiva : Exception
+    {
+        public CuentaNoActiva(Estado estado) : base($"No se puede operar con la cuenta {estado}") { }
+    }
+}
